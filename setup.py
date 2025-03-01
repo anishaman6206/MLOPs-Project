78a66c6aa8c1f7a098d2df4b9732d47fa3b08dcd
anishaman6206 <anishaman6206@gmail.com>
2025-03-01 22:20:12 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Anish Aman",
+    author_email="anishaman6206@gmail.com",
+    packages=find_packages()
+)
