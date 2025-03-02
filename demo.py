ffd0fe803ecc30ebde90936ee93f79ffcecfe1d3
anishaman6206 <anishaman6206@gmail.com>
2025-03-02 16:29:53 +0530

    data ingestion added

--- demo.py
+++ demo.py
@@ -1,4 +1,9 @@
-from src.logger import logging
+# from src.logger import logging
 
-logging.debug("This is a debug message")
-logging.info("This is an info message")
+# logging.debug("This is a debug message")
+# logging.info("This is an info message")
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
